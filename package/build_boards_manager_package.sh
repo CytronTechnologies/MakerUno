77d45ba5fb01d36603f18e76509ffcaf8014b9a1
Ng Beng Chet <bengchet@cytron.com.my>
2018-05-02 09:02:08 +0800

    Update build_boards_manager_package.sh

--- package/build_boards_manager_package.sh
+++ package/build_boards_manager_package.sh
@@ -46,7 +46,7 @@ cp bootloaders/optiboot/optiboot_makeruno.hex $srcdir/$outdir/bootloaders/optibo
 cp boards.txt $srcdir/$outdir/boards.txt
 find $srcdir/$outdir/platform.txt -exec sed -i 's|name=Arduino AVR Boards|name=Maker Uno|g' {} \;
 find $srcdir/$outdir/platform.txt -exec sed -i 's|version='$ARDUINO_VER'|version='$ver'|g' {} \;
-git clone https://github.com/CytronTechnologies/MakerUno_Examples.git $srcdir/$outdir/libraries/
+git clone https://github.com/CytronTechnologies/MakerUno_Examples.git $srcdir/$outdir/libraries/MakerUno_Examples
 ls -a $srcdir/$outdir/libraries/MakerUno_Examples/*
 pushd package/versions/$ver
 echo "Making $package_name.zip"
